06851ca5c60263f223cffc13aaaa720dab147ff1
SergioMorales23 <sertin468@gmail.com>
2025-02-15 10:40:16 -0300

    Primer commit

---
+++ src/main/java/ar/edu/unju/edm/trabajofebrero/TrabajofebreroApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm.trabajofebrero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajofebreroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajofebreroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/edm/trabajofebrero/TrabajofebreroApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm.trabajofebrero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajofebreroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
